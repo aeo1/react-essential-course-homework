1feeab656c8867d634cb6087426d41091ce6ac95
Sergey Gromkov <sgromkov@gmail.com>
2017-11-09 11:38:39 +0300

    Revert "Lesson 3. Homework 3."

--- 03-environment/homework/03/src/main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import FinanceApp from './components/FinanceApp.jsx';
-//import 'semantic-ui-css/semantic.min.css';
-
-ReactDOM.render(
-    <FinanceApp />,
-    document.getElementById('content')
-);
